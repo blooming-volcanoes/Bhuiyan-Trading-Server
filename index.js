9f79e76554335df89106280bdc191549a96cadc3
Awal hossain <waqin21@gmail.com>
2022-09-01 10:23:35 +0600

    build: folder structure consist of MVC pattern 🚀🚀

---
+++ index.js
@@ -0,0 +1,43 @@
+/* eslint-disable prettier/prettier */
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+
+
+const app = require('express')();
+
+const port = process.env.PORT || 3000;
+
+// middleware
+// const errorMiddleware = require('./middleware/error');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// connect with MySql DB
+
+
+// Socket
+const http = require('http').createServer(app);
+
+
+
+
+
+
+app.get('/hi', (req, res) => {
+    res.send('hi');
+    console.log('Hi');
+});
+
+app.get('/', (req, res) => {
+    res.send('hello');
+});
+
+// app.use(errorMiddleware);
+
+http.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
