1448aad94d9e7cfb5a7ccd246ab7dfebe41ed546
Awal hossain <waqin21@gmail.com>
2022-09-01 17:30:36 +0600

    most commor error catching formula has  been created 🎆🎆

--- middleware/error.js
+++ middleware/error.js
@@ -17,6 +17,16 @@ module.exports = (error, req, res, next)=>{
             const message =` Authentication failed against database server at {database_host}` ;
             err = new ErrorHandler(message, 400);
         }
+        if(err.code === 'P1008'){
+            const message =` Operations timed out after {time}` ;
+            err = new ErrorHandler(message, 400);
+        }
+        if(err.code === 'P1013'){
+            const message =` The provided database string is invalid. {details}` ;
+            err = new ErrorHandler(message, 400);
+        }
+
+
     }
 
 
