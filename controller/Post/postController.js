e509371a0aef251618a3d638125f5d6c7a1b1a69
Awal hossain <waqin21@gmail.com>
2022-10-12 19:09:17 +0600

    feat: node-cron schedule update ❤️💯

--- controller/Post/postController.js
+++ controller/Post/postController.js
@@ -1,23 +1,38 @@
 const catchAsyncError = require('../../middleware/catchAsyncError');
 const nodeCron = require("node-cron");
-
-const log4js = require('log4js');
-const logger = log4js.getLogger();
+const EventEmitter = require('events');
+const event = new EventEmitter();
 
 const sendToken = require('../../lib/jwt.js');
 const db = require('../../db/connection')
 
 exports.createPost =  catchAsyncError(async (req, res, next) => {
     const {title, categoryId, postDesc, featureImg, imgCaption, focusKey, metaDesc, alt, status, } = req.body;
-    
+    const date = new Date(0, 47,18, 12, 10, 3);
+
+    console.log(date,"ch");
     if(status == 'now'){
         console.log("posting now");
     }else {
-        const job = nodeCron.schedule("*/2 * * * *", ()=>schedulePost(req,res));
+        const job = nodeCron.schedule("0 1 19 12 October Wednesday", async()=>{
+            
+            await schedulePost(req,res)
+            event.emit('JOB COMPLETED');
+        });
+        
+
+        event.on('JOB COMPLETED', () => {
+            console.log('Job done!');
+            job.stop();
+        });
     }
 
   
  })
+
+
+ // Listen to when a 'JOB COMPLETED' event is emitted and stop the task
+
 
 
 
@@ -36,6 +51,7 @@ exports.createPost =  catchAsyncError(async (req, res, next) => {
 
 
 
+//Sechudel post time
  async function schedulePost(req, res){
     console.log("here", req.body);
     const {title, categoryId, postDesc, featureImg, imgCaption, focusKey, metaDesc, alt, status, } = req.body;
@@ -51,7 +67,6 @@ exports.createPost =  catchAsyncError(async (req, res, next) => {
 
     db.query(query, [title, categoryId, postDesc, featureImg, imgCaption, focusKey, metaDesc, alt, status, slug], (err, result) => {
         if (!err) {
-            logger.debug(result,"from create post");
             return res.status(200).json({ msg: "Post created Successfully" })
         } else {
             return res.status(500).json(err)
