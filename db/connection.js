810374f40e5d3f0c4a52763b35508f40abf90852
Awal hossain <waqin21@gmail.com>
2022-10-13 14:55:38 +0600

    blog post schedule updae👍

--- db/connection.js
+++ db/connection.js
@@ -11,22 +11,22 @@ const logger = log4js.getLogger();
 //     password: "LVmd44OGibG6oYoGr6Ad",
 //     database: "railway"
 // })
-// let connection = mysql.createPool({
-//     port: 3306,
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'password',
-//     database: 'buiyandb',
-// });
-
-
 let connection = mysql.createPool({
-    port:3306,
-    host:"localhost",
-    user: "bhuiyantrad_user",
-    password: "@xWgFOUfX4r9",
-    database: "bhuiyantrad_test"
-})
+    port: 3306,
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'buiyandb',
+});
+
+
+// let connection = mysql.createPool({
+//     port:3306,
+//     host:"localhost",
+//     user: "bhuiyantrad_user",
+//     password: "@xWgFOUfX4r9",
+//     database: "bhuiyantrad_test"
+// })
 
 
 connection.getConnection((err, result) => {
